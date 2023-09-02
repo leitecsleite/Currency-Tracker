5ebb2f4783c43f6a3cf3e1ab43813366fbcc06ae
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-09-02 17:17:50 -0300

    Added attributes to improve user sensitivity in the tags.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,56 @@
-import Image from "next/image"
+import Image from "next/image";
 
-
-export default function Header(){
-    return(
-        <>
-         <header className="flex items-center justify-center bg-gray-950">
-             <section className="flex justify-between w-10/12 py-7">
-                <div className="w-1/4">
-                    <Image 
-                    src='/logo.svg'
-                    alt="Currency Logo"
-                    className="dark:invert"
-                    width={40}
-                    height={40}
-                    priority
-                    />
-                </div>
-                <nav className="w-1/2 flex items-center justify-center">
-                    <ul className="text-white  text-20 font-light leading-41.143 w-full flex justify-between ">
-                        <li><a href="/">Home</a></li>
-                         <li><a href="/">Conversor</a></li>
-                         <li><a href="/">Sobre</a></li>
-                         <li><a href="/">Contato</a></li>
-                    </ul>
-                </nav>
-                <div className="w-1/4"></div>
-             </section>
-         </header>
-        </>
-    )
+export default function Header() {
+  return (
+    <>
+      <header
+        aria-label="Cabeçalho do site"
+        className="flex items-center justify-center bg-gray-950"
+      >
+        <section className="flex justify-between w-10/12 py-7">
+          <div aria-label="Logo" className="w-1/4">
+            <Image
+              src="/logo.svg"
+              alt="Currency Logo"
+              className="dark:invert"
+              width={40}
+              height={40}
+              priority
+            />
+          </div>
+          <nav
+            role="navigation"
+            className="w-1/2 flex items-center justify-center"
+          >
+            <ul
+              aria-label="Itens do Menu"
+              className="text-white  text-20 font-light leading-41.143 w-full flex justify-between "
+            >
+              <li aria-label="Link para a página inicial">
+                <a title="Página inicial" href="/">
+                  Home
+                </a>
+              </li>
+              <li aria-label="Link para a conversor">
+                <a title="Página de conversor" href="/">
+                  Conversor
+                </a>
+              </li>
+              <li aria-label="Link para a página sobre nós">
+                <a title="Página saiba mais soobre a nossa empresa" href="/">
+                  Sobre
+                </a>
+              </li>
+              <li aria-label="Link para a página contato">
+                <a title="Página de Contato" href="/">
+                  Contato
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="w-1/4"></div>
+        </section>
+      </header>
+    </>
+  );
 }
