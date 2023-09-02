45f65adfd87b3c6e86deb858c6b2b0b924c0d834
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-09-02 17:16:34 -0300

    add banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,23 +1,21 @@
 import Image from "next/image";
 
-
-export default function Banner(){
-    return(
-        <section className="bg-gradient-to-r from-gray-900 to-transparent">
-            <Image
-              src='/Banner.svg'
-              alt="Banner principal"
-              className="dark:invert"
-              sizes="100vw"
-              // Make the image display full width
-              width={1440}
-              height={405}
-              style={{
-                width: '100%',
-                height: 'auto',
-              }}
-             
-            />
-        </section>
-    )
+export default function Banner() {
+  return (
+    <section className="bg-gradient-to-r from-gray-900 to-transparent" aria-label="Banner-principal">
+      <Image
+        src="/Banner.svg"
+        alt="Banner principal"
+        className="dark:invert"
+        sizes="100vw"
+        // Make the image display full width
+        width={1440}
+        height={405}
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+      />
+    </section>
+  );
 }
