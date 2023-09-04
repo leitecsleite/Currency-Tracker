6f4fe8be141758f5682382ac6c851338490bc26c
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-09-04 17:03:32 -0300

    create section about last update

--- src/components/LastUpdate/index.tsx
+++ src/components/LastUpdate/index.tsx
@@ -4,8 +4,14 @@
 export default function LastUpdate(){
     return(
      <>
-      <section aria-label="Última atualização">
-
+      <section aria-label="Última atualização" className=" flex justify-center align-middle pt-5 ">
+        <div className="max-w-2xl flex justify-center items-center">
+            <span className="circle-animation"></span>
+             <div className="flex justify-between text-gray-400 font-poppins text-2xl font-normal leading-10">
+                <p className="flex items-center mr-2 ">Última atualização às </p>
+                <time className="flex items-center"> 23:56h</time>
+             </div>
+        </div>
       </section>
      </>
     )
