2e339cb58afae54b2521248c24f5d48fb2518cad
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-09-04 17:57:20 -0300

    add atribute test

--- src/components/LastUpdate/index.tsx
+++ src/components/LastUpdate/index.tsx
@@ -1,12 +1,10 @@
 
-
-
 export default function LastUpdate(){
     return(
      <>
-      <section aria-label="Última atualização" className=" flex justify-center align-middle pt-5 ">
+      <section data-testid="last update" aria-label="Última atualização" className=" flex justify-center align-middle pt-5 ">
         <div className="max-w-2xl flex justify-center items-center">
-            <span className="circle-animation"></span>
+            <span data-testid="circle-animation" className="circle-animation"></span>
              <div className="flex justify-between text-gray-400 font-poppins text-2xl font-normal leading-10">
                 <p className="flex items-center mr-2 ">Última atualização às </p>
                 <time className="flex items-center"> 23:56h</time>
