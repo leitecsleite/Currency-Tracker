04dc66cb5a01fcd4fdc8fe30cbc382ff2f87b0e8
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-09-04 17:04:11 -0300

    section main content

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import LastUpdate from "../LastUpdate";
+
+export default function Main() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center bg-custom">
+        <LastUpdate />
+      </main>
+    </>
+  );
+}
