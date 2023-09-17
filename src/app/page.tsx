46dcc35f5f31072594ab9eb70cbcbe52cb418dc0
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-09-17 18:14:24 -0300

    remove context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import Banner from "@/components/Banner";
 import Main from "@/components/Main";
-import PhotosProvider from "@/context/contextCash";
-
-
 
 export default  function Home() {
 
@@ -10,10 +7,7 @@ export default  function Home() {
    <>
      <section className="flex justify-center w-full flex-col ">
        <Banner/>
-       <PhotosProvider>
          <Main/>
-       </PhotosProvider>
-      
      </section>
    </>
   )
