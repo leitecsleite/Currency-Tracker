1a9f3957b1dd4ea44a2ec72681e3f3079c77fd64
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-09-19 09:19:06 -0300

    add lib React Skeleton

--- src/components/Currency/index.tsx
+++ src/components/Currency/index.tsx
@@ -1,5 +1,10 @@
+"use client";
 
 import Currency from "@/lib/data";
+import { useEffect, useState } from "react";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
+
 type CurrencyData = {
   fromCurrency: string;
   toCurrency: string;
@@ -12,45 +17,95 @@ type CurrencyData = {
   updatedAtDate: string;
 };
 
-export default async function Cards({}) {
-  const currencyData: CurrencyData[] = await Currency();
+export default function Cards() {
+  const [currencyData, setCurrencyData] = useState<CurrencyData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isMoreCards, setIsMoreCards] = useState<number>(3);
 
-  if (!currencyData) {
-    return null;
+  useEffect(() => {
+    async function getData() {
+      const response = await Currency();
+
+      setCurrencyData(response);
+      setIsLoading(true);
+    }
+    getData();
+  }, []);
+
+  function handleMoreCard() {
+    if (isMoreCards < currencyData.length) {
+      setIsMoreCards((cards) => cards + 3);
+    }
   }
 
   return (
     <>
-      <section className="flex justify-between w-full flex-wrap">
-        <div className="flex justify-between flex-col">
-          {currencyData.length > 0 ? (
-            currencyData.map((currency, index) => (
-              <>
-                <div
-                  key={index}
-                  className="flex w-520 h-36 px-32 py-30 items-center gap-y-6 rounded-lg border border-gray-700 bg-gray-900"
-                >
-                  <div className="w-279.287 h-24  flex justify-center align-middle flex-col">
-                    <p className="text-gray-400 font-poppins text-base font-normal leading-49.371">
-                      {currency.name.split("/")[0].trim()}
-                    </p>
-                    <p className="text-gray-400 font-poppins text-base font-light leading-10">
-                      R${" "}
-                      {parseFloat(currency.askPrice)
-                        .toFixed(2)
-                        .toString()
-                        .replace(".", ",")}
-                    </p>
+      {isLoading ? (
+        <section className="flex justify-between w-full flex-wrap">
+          <div className="flex justify-between flex-wrap">
+            {currencyData.length > 0 ? (
+              currencyData.slice(0, isMoreCards).map((currency, index) => (
+                <>
+                  <div
+                    key={index}
+                    className="flex w-1/3 h-36 px-32 py-30 items-center gap-y-6 rounded-lg border border-gray-700 bg-gray-900 box-border"
+                  >
+                    <div className="w-279.287 h-24  flex justify-center align-middle flex-col">
+                      <p className="text-gray-400 font-poppins text-base font-normal leading-49.371">
+                        {currency.name.split("/")[0].trim()}
+                      </p>
+                      <p className="text-gray-400 font-poppins text-base font-light leading-10">
+                        R${" "}
+                        {parseFloat(currency.askPrice)
+                          .toFixed(2)
+                          .toString()
+                          .replace(".", ",")}
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <br></br>
-              </>
-            ))
-          ) : (
-            <h1>Não tem dados ...</h1>
-          )}
-        </div>
-      </section>
+                  <br></br>
+                </>
+              ))
+            ) : (
+              <h1>Não tem dados ...</h1>
+            )}
+          </div>
+        </section>
+      ) : (
+        <section className="flex justify-between w-full flex-wrap">
+          <div className="flex justify-between flex-wrap">
+            <Stack spacing={1}>
+              <Skeleton
+                variant="rounded"
+                width={366}
+                height={144}
+                sx={{ bgcolor: "grey.900" }}
+              />
+            </Stack>
+            <Stack spacing={1}>
+              <Skeleton
+                variant="rounded"
+                width={366}
+                height={144}
+                sx={{ bgcolor: "grey.900" }}
+              />
+            </Stack>
+            <Stack spacing={1}>
+              <Skeleton
+                variant="rounded"
+                width={366}
+                height={144}
+                sx={{ bgcolor: "grey.900" }}
+              />
+            </Stack>
+          </div>
+        </section>
+      )}
+      {isMoreCards < currencyData.length && (
+        <button onClick={handleMoreCard} className="text-gray-400">
+          VER MAIS
+        </button>
+      )}
     </>
   );
 }
