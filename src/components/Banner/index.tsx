60fe7d6338c5c6e7952a8b14778ef6cafeeeffcf
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-09-20 13:09:59 -0300

    add Banner Mobile

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,11 +2,28 @@ import Image from "next/image";
 
 export default function Banner() {
   return (
-    <section className="bg-gradient-to-r from-gray-900 to-transparent" aria-label="Banner-principal">
+    <section
+      className="bg-gradient-to-r from-gray-900 to-transparent"
+      aria-label="Banner-principal"
+    >
       <Image
         src="/Banner.svg"
         alt="Banner principal"
-        className="dark:invert"
+        className="dark:invert md:block hidden"
+        sizes="100vw"
+        // Make the image display full width
+        width={1440}
+        height={405}
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+      />
+
+      <Image
+        src="/Banner-Mobile.svg"
+        alt="Banner principal"
+        className="dark:invert md:hidden "
         sizes="100vw"
         // Make the image display full width
         width={1440}
