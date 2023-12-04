21bcb6c2e29db74589b63eddc5132954dfce73e3
Lucas <carvalho1104@hotmail.com>
2023-12-04 20:26:39 -0300

    feat: update API

--- src/components/Currency/index.tsx
+++ src/components/Currency/index.tsx
@@ -6,15 +6,8 @@ import Skeleton from "@mui/material/Skeleton";
 import Stack from "@mui/material/Stack";
 
 type CurrencyData = {
-  fromCurrency: string;
-  toCurrency: string;
-  name: string;
-  high: string;
-  low: string;
-  bidVariation: string;
-  askPrice: string;
-  bidPrice: string;
-  updatedAtDate: string;
+  code: string;
+  value: number;
 };
 
 export default function Cards() {
@@ -24,22 +17,22 @@ export default function Cards() {
 
   useEffect(() => {
     async function getData() {
-      const response = await Currency();
-      const data: CurrencyData[] = response.filter(
-        (res: CurrencyData) =>
-          res.fromCurrency === "USD" ||
-          res.fromCurrency === "GBT" ||
-          res.fromCurrency === "BTC" ||
-          res.fromCurrency === "EUR" ||
-          res.fromCurrency === "JPY" ||
-          res.fromCurrency === "AUD" ||
-          res.fromCurrency === "CAD" ||
-          res.fromCurrency === "ARS" ||
-          res.fromCurrency === "CNY"
-      );
-      setCurrencyData(data);
-      setIsLoading(true);
+      try {
+        const response = await Currency();
+        const filteredCurrencies: CurrencyData[] = Object.entries(response.data)
+          .filter(([code, _]) =>
+            ["USD", "GBT", "BTC", "EUR", "JPY", "AUD", "CAD", "ARS", "CNY"].includes(code)
+          )
+          .map(([code, value]) => ({ code, value }));
+
+        setCurrencyData(filteredCurrencies);
+        setIsLoading(true);
+      } catch (error) {
+        console.error("Error fetching currency data:", error);
+        // Handle error state if needed
+      }
     }
+
     getData();
   }, []);
 
@@ -48,52 +41,43 @@ export default function Cards() {
       setIsMoreCards((cards) => cards + 2);
     }
   }
-
+ console.log(currencyData)
   return (
     <>
       {isLoading ? (
         <section aria-label="Cotação da Moeda" className="flex justify-center my-6">
-          <div
-            className="flex w-80% max-w-screen-2xl justify-center align-middle
-           flex-wrap gap-3 m-4 "
-          >
+          <div className="flex w-80% max-w-screen-2xl justify-center align-middle flex-wrap gap-3 m-4 ">
             {currencyData.length > 0 ? (
               currencyData.slice(0, isMoreCards).map((currency, index) => (
-                <>
-                  <div
-                    aria-label="cartão cotação" 
-                    key={index}
-                    className="flex  w-520 h-36 px-32 py-30 items-center justify-between rounded-lg border border-gray-700 bg-gray-900 box-border max-sm:w-96 max-sm:px-10 max-sm:py-10 "
-                  >
-                    <div className="flex align-middle justify-center ">
-                      <img
-                        className=" w-12 h-12 box-border"
-                        src={`https://res.cloudinary.com/djugggwq3/image/upload/v1695150583/${currency.fromCurrency}.png`}
-                        alt={currency.fromCurrency}
-                      />
-                    </div>
-                    <div aria-label="nome da moeda" className="w-279.287 h-24  flex justify-center align-middle flex-col">
-                      {currency.name ===
-                      "Dólar Americano/Real Brasileiro Turismo" ? (
-                        <p className="text-gray-400 font-poppins text-base font-normal leading-49.371">
-                          {currency.name.split("/")[0].trim()} Turismo
-                        </p>
-                      ) : (
-                        <p aria-label="nome da moeda" className="text-gray-400 font-poppins text-base font-normal leading-49.371">
-                          {currency.name.split("/")[0].trim()}
-                        </p>
-                      )}
-
-                      <p aria-label="valor da moeda em Real Brasileiro" className="text-gray-400 font-poppins text-base font-light leading-10">
-                        R${" "}
-                        {parseFloat(currency.askPrice)
-                          .toFixed(2)
-                          .toString()
-                          .replace(".", ",")}
-                      </p>
-                    </div>
+                <div
+                  aria-label="cartão cotação"
+                  key={index}
+                  className="flex w-520 h-36 px-32 py-30 items-center justify-between rounded-lg border border-gray-700 bg-gray-900 box-border max-sm:w-96 max-sm:px-10 max-sm:py-10 "
+                >
+                  <div className="flex align-middle justify-center ">
+                    {/* You can use the cloudinary URL directly here if needed */}
+                    <img
+                      className="w-12 h-12 box-border"
+                      src={`https://res.cloudinary.com/djugggwq3/image/upload/v1695150583/${currency.code}.png`}
+                      alt={currency.code}
+                    />
                   </div>
-                </>
+                  <div aria-label="nome da moeda" className="w-279.287 h-24 flex justify-center align-middle flex-col">
+                    <p
+                      aria-label="nome da moeda"
+                      className="text-gray-400 font-poppins text-base font-normal leading-49.371"
+                    >
+                      {currency.code}
+                    </p>
+                    <p
+                      aria-label="valor da moeda em Real Brasileiro"
+                      className="text-gray-400 font-poppins text-base font-light leading-10"
+                    >
+                      R${" "}
+                      {parseFloat(currency.value.value.toFixed(2)).toString().replace(".", ",")}
+                    </p>
+                  </div>
+                </div>
               ))
             ) : (
               <h1>Não tem dados ...</h1>
@@ -102,38 +86,17 @@ export default function Cards() {
         </section>
       ) : (
         <section aria-label="Carregando a seção" className="flex justify-center my-6">
-          <div aria-label="Carregando de cards"className="flex  w-80% max-w-screen-2xl justify-center flex-wrap gap-3 m-4">
+          <div aria-label="Carregando de cards" className="flex w-80% max-w-screen-2xl justify-center flex-wrap gap-3 m-4">
             <Stack spacing={1}>
-              <Skeleton
-                variant="rounded"
-                height={144}
-                className=" w-520 max-sm:w-96"
-                sx={{ bgcolor: "grey.900" }}
-              />
-            </Stack>
-            <Stack spacing={1}>
-              <Skeleton
-                variant="rounded"
-                className=" w-520 max-sm:w-96"
-                height={144}
-                sx={{ bgcolor: "grey.900" }}
-              />
-            </Stack>
-            <Stack spacing={1}>
-              <Skeleton
-                variant="rounded"
-                className=" w-520 max-sm:w-96"
-                height={144}
-                sx={{ bgcolor: "grey.900" }}
-              />
-            </Stack>
-            <Stack spacing={1}>
-              <Skeleton
-                variant="rounded"
-                className=" w-520 max-sm:w-96"
-                height={144}
-                sx={{ bgcolor: "grey.900" }}
-              />
+              {[...Array(4)].map((_, index) => (
+                <Skeleton
+                  key={index}
+                  variant="rounded"
+                  height={144}
+                  className="w-520 max-sm:w-96"
+                  sx={{ bgcolor: "grey.900" }}
+                />
+              ))}
             </Stack>
           </div>
         </section>
--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -20,7 +20,7 @@ export default async function Currency() {
   try {
     const currencyList: any = await fetchCurrencies();
     const response = await fetch(
-      `https://brapi.dev/api/v2/currency?currency=${currencyList}`,
+      `https://api.currencyapi.com/v3/latest?apikey=cur_live_xXMjsaE2GcJsJLJ1OyZ2io5CYCVof70Czo9hLPp8&base_currency=USD`,
       {
         next: {
           revalidate: 60000,
@@ -29,9 +29,10 @@ export default async function Currency() {
     );
 
     const data = await response.json(); 
-    const { currency } = data;
+    console.log(data)
+   
 
-    return  currency ;
+    return data ;
   } catch (error) {
     throw new Error('Failed to fetch data')
   }
