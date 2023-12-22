a29379a04af4115bbcd0fb5918550a8a3113fbcb
Lucas <carvalho1104@hotmail.com>
2023-12-22 19:25:38 -0300

    feat: anuncio do google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 
 import Banner from "@/components/Banner";
 import Main from "@/components/Main";
+import Head from 'next/head';
 
 export default  function Home() {
 
   return (
    <>
+     <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7860211370494598"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
      <section className="flex justify-center w-full flex-col bg-custom">
         <Banner/>
         <Main/>
