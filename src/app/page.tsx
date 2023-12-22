217d493cf31d68dff36e95103fcc4123763de89c
Lucas <carvalho1104@hotmail.com>
2023-12-22 19:42:41 -0300

    fix: anuncio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
-
 import Banner from "@/components/Banner";
 import Main from "@/components/Main";
 import Head from 'next/head';
 
-export default  function Home() {
-
+export default function Home() {
   return (
-   <>
-     <Head>
-        <meta name="google-adsense-account" content="ca-pub-7860211370494598"></meta>
+    <>
+      <Head>
+        <title>Sua Página</title>
       </Head>
-     <section className="flex justify-center w-full flex-col bg-custom">
-        <Banner/>
-        <Main/>
-     </section>
-   </>
-  )
+      <section className="flex justify-center w-full flex-col bg-custom">
+        <Banner />
+        <Main />
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-7860211370494598"
+          data-ad-slot="SEU_SLOT_ID"
+          data-ad-format="auto"
+        ></ins>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
+      </section>
+    </>
+  );
 }
